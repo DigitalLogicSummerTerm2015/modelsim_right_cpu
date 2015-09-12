ec32e2eec655ce32fb94a151de06ca672eff9571
Thomas Lee <lisihan969@gmail.com>
2015-09-12 20:51:21 +0800

    Use lastest rom.v

--- rom.v
+++ rom.v
@@ -1,12 +1,13 @@
 `timescale 1ns/1ps
 
 module ROM(addr, data);
-input [31  :0] addr;
+input [31:0] addr;
 output [31:0] data;
 
 localparam ROM_SIZE = 32;
 
-reg [31:0] dat _DATA[ROM_SIZE-1:0];
+reg [31:0] data;
+reg [31:0] ROM_DATA[ROM_SIZE-1:0];
 
 always@(*)
     case(addr[9:2])  // Address Must Be Word Aligned.
